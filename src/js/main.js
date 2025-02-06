4fa5718939b65dfe8168e6ccea734721bea88bb9
Jakub <jakub2002t@gmail.com>
2025-02-06 22:31:02 +0100

    added others offers

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,18 @@
 const navMobile = document.querySelector(".nav__mobile");
-const burgerBtn = document.querySelector(".burger-btn");
 const navMobileLinks = document.querySelectorAll(".nav__moblie-link");
+
+const burgerBtn = document.querySelector(".burger-btn");
 
 const showMobileNav = () => {
 	navMobile.classList.toggle("nav__mobile--active");
+	burgerBtn.classList.toggle("burger-btn--active")
 };
 
 burgerBtn.addEventListener("click", showMobileNav);
 navMobileLinks.forEach((link) => {
-	link.addEventListener("click", () =>
+	link.addEventListener("click", () => {
 		navMobile.classList.remove("nav__mobile--active")
+		burgerBtn.classList.remove("burger-btn--active")
+	}
 	);
 });
