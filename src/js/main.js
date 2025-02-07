295563c51df2d9d2a9aec39a76dd81f5e2dc9b0d
Jakub <jakub2002t@gmail.com>
2025-02-07 19:28:29 +0100

    added mixin, fixed nav and added contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
 const navMobile = document.querySelector(".nav__mobile");
 const navMobileLinks = document.querySelectorAll(".nav__moblie-link");
-
 const burgerBtn = document.querySelector(".burger-btn");
+const navBg = document.querySelector(".nav__logo");
 
 const showMobileNav = () => {
 	navMobile.classList.toggle("nav__mobile--active");
-	burgerBtn.classList.toggle("burger-btn--active")
+	burgerBtn.classList.toggle("burger-btn--active");
+
+	
 };
+
+const handleShadow = () => {
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 50) {
+			navBg.classList.add("shadow-bg");
+		} else {
+			navBg.classList.remove("shadow-bg");
+		}
+	});
+};
+handleShadow();
 
 burgerBtn.addEventListener("click", showMobileNav);
 navMobileLinks.forEach((link) => {
 	link.addEventListener("click", () => {
-		navMobile.classList.remove("nav__mobile--active")
-		burgerBtn.classList.remove("burger-btn--active")
-	}
-	);
+		navMobile.classList.remove("nav__mobile--active");
+		burgerBtn.classList.remove("burger-btn--active");
+	});
 });
